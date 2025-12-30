632b7afb607b7458ff698927dc34cb60e0c47b8d
jaredricardo <jaredricardo94@gmail.com>
2025-12-30 04:18:15 -0500

    further header work

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 import './main.css'
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Theme initialized for test 77')
+    // initialize event listeners
+    initializeMobileMenuButton()
 })
 
+
+class ThemeUnderlay extends HTMLElement {
+    constructor() {
+        super()
+        this.addEventListener('click',  toggleUnderlay)
+    }
+}
+
+
+function toggleUnderlay() {
+    document.body.classList.remove('underlay-active')
+    document.querySelector('theme-underlay').classList.remove('active' )
+}
+
+function initializeMobileMenuButton() {
+    const mobileMenuButton = document.getElementById('mobile-menu-button')
+    if (!mobileMenuButton) return
+    mobileMenuButton.addEventListener('click', () => {
+        document.body.classList.add('underlay-active')
+        document.querySelector('theme-underlay').classList.add('active' )
+    })
+}
+
+
+customElements.define('theme-underlay', ThemeUnderlay)
